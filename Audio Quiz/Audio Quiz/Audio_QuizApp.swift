a996960815044ff8ecc9223fc0bef25743f2c9c1
Nlemadim <54709652+Nlemadim@users.noreply.github.com>
2024-01-18 08:59:17 -0500

    Add files via upload

---
+++ Audio Quiz/Audio Quiz/Audio_QuizApp.swift
@@ -0,0 +1,19 @@
+//
+//  Audio_QuizApp.swift
+//  Audio Quiz
+//
+//  Created by Tony Nlemadim on 1/18/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Audio_QuizApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Audio Quiz/Audio Quiz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Audio Quiz
+//
+//  Created by Tony Nlemadim on 1/18/24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    
+
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+        
+}
